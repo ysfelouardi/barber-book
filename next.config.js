5312a6494a5b69a18a99f5f1cb85092addd02e92
ysfelouardi <youssef.94.elouardi@gmail.com>
2025-06-13 19:38:38 +0200

    fix swagger

--- next.config.js
+++ next.config.js
@@ -30,7 +30,7 @@ const nextConfig = {
           {
             key: 'Content-Security-Policy',
             value:
-              "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://apis.google.com https://www.gstatic.com https://accounts.google.com https://www.google.com https://www.recaptcha.net https://recaptcha.net; style-src 'self' 'unsafe-inline' https://accounts.google.com https://www.gstatic.com; img-src 'self' data: https: *.googleusercontent.com; font-src 'self' data: https://fonts.gstatic.com; connect-src 'self' https: wss: *.googleapis.com *.firebaseio.com *.cloudfunctions.net; frame-src https://accounts.google.com https://*.firebaseapp.com https://www.google.com https://www.recaptcha.net https://recaptcha.net; frame-ancestors 'none';",
+              "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://apis.google.com https://www.gstatic.com https://accounts.google.com https://www.google.com https://www.recaptcha.net https://recaptcha.net https://unpkg.com; style-src 'self' 'unsafe-inline' https://accounts.google.com https://www.gstatic.com https://unpkg.com; img-src 'self' data: https: *.googleusercontent.com; font-src 'self' data: https://fonts.gstatic.com https://unpkg.com; connect-src 'self' https: wss: *.googleapis.com *.firebaseio.com *.cloudfunctions.net https://unpkg.com; frame-src https://accounts.google.com https://*.firebaseapp.com https://www.google.com https://www.recaptcha.net https://recaptcha.net; frame-ancestors 'none';",
           },
         ],
       },
